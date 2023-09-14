eab0a08a958efac102e4e49b2a55988ed56c02ce
dhruveel10 <dhruveel.chouhan.3@gmail.com>
2023-09-14 12:06:13 -0400

    media query changes

--- assets/js/project.js
+++ assets/js/project.js
@@ -26,7 +26,7 @@ const projects = [
   {
     title: "Vid2Music",
     cardImage: "assets/images/project-page/vid2music.jpeg",
-    description: "An ML model that recommends music based on emotions.",
+    description: "ML model that recommends music based on emotions.",
     Githublink: "https://github.com/dhruveel10/Vid2Music",
   },
   {
@@ -110,9 +110,9 @@ const showCards = () => {
             <div class="data">
               <div class="content">
                 <div class="title-div">
-                  <h1 class="title"><a href="#">${title}</a></h1>
+                  <h6 class="title"><a href="#">${title}</a></h6>
                 </div>
-                <div class="description" style="margin: 0;">${description}</div>
+                <div class="description" style="margin: 0; font-size:14px">${description}</div>
                 <ul class="menu-content"><br>
                   <li><a href="${Githublink}" class="social-icon"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" viewBox="0 0 30 28" fill="none" stroke="#fff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-github"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg></a></li>
                 </ul>
